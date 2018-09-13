9580120f8dcbae02d7a8e6d2a886a83eb9483eee
tomoyuen <wisyue@yahoo.com>
2018-09-13 18:49:22 +0800

    try mobx examples

--- src/renderer.jsx
+++ src/renderer.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { observable, action } from 'mobx';
+import {
+  observable,
+  action,
+  computed,
+  autorun,
+} from 'mobx';
 import { observer, PropTypes } from 'mobx-react';
 
 // https://mobx.js.org/intro/overview.html
@@ -8,6 +13,85 @@ import { observer, PropTypes } from 'mobx-react';
 const appState = observable({
   timer: 0,
 });
+
+class OrderLine {
+  @observable price = 0;
+
+  @observable amount = 1;
+
+  constructor(price) {
+    this.price = price;
+  }
+
+  @computed get total() {
+    return this.price * this.amount;
+  }
+}
+
+class Foo {
+  @observable length = 2;
+
+  @computed get squared() {
+    return this.length * this.length;
+  }
+
+  set squared(value) {
+    this.length = Math.sqrt(value);
+  }
+}
+
+const foo = new Foo();
+console.log(foo.squared);
+
+const person = observable({
+  name: 'John',
+  age: 42,
+  showAge: false,
+
+  get labelText() {
+    return this.showAge ? `${this.name} (age: ${this.age})` : this.name;
+  },
+  setAge(age) {
+    this.age = age;
+  },
+  toggleAgeDisplay() {
+    this.showAge = !this.showAge;
+  },
+}, {
+  setAge: action,
+  toggleAgeDisplay: action,
+});
+
+autorun(() => console.log(person.labelText));
+person.toggleAgeDisplay();
+person.name = 'Dave';
+person.toggleAgeDisplay();
+person.setAge(21);
+
+const todos = observable([
+  { title: 'Spoil tea', completed: true },
+  { title: 'Make coffee', completed: false },
+]);
+
+autorun(() => {
+  console.log('Remaining:', todos
+    .filter(todo => !todo.completed)
+    .map(todo => todo.title)
+    .join(', '));
+});
+
+todos[0].completed = false;
+todos.push({ title: 'Take a rest', completed: false });
+todos.shift();
+
+const cityName = observable.box('Vienna');
+console.log(cityName.get());
+
+cityName.observe((change) => {
+  console.log(change.oldValue, '->', change.newValue);
+});
+
+cityName.set('Amsterdam');
 
 @observer class TimerView extends Component {
   static propTypes = {
@@ -27,10 +111,15 @@ const appState = observable({
     const { state } = this.props;
     const { timer } = state;
     return (
-      <button type="button" onClick={() => this.onReset()}>
-        Seconds passed:
-        { timer }
-      </button>
+      <div>
+        <h1>
+          Seconds passed:
+          { timer }
+        </h1>
+        <h2>{(new OrderLine(4, 5)).total}</h2>
+        <br />
+        <button type="button" onClick={() => this.onReset()}>reset</button>
+      </div>
     );
   }
 }
